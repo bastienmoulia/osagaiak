58bb4657072ee419b2f44199fb903b2a3057a071
Bastien <bmoulia@gmail.com>
2024-12-10 19:48:34 +0100

    test deploy gh pages

--- apps/osagaiak/src/app/app.config.ts
+++ apps/osagaiak/src/app/app.config.ts
@@ -1,14 +1,13 @@
-import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideExperimentalZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
-import {
-  provideClientHydration,
-  withEventReplay,
-} from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideClientHydration(withEventReplay()),
+    //provideClientHydration(withEventReplay()),
     provideExperimentalZonelessChangeDetection(),
     provideRouter(appRoutes),
   ],
